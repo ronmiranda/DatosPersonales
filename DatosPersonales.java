d821bcf3164528569e3c04f41ca37c52c69f274a
ronmiranda <37202978+ronmiranda@users.noreply.github.com>
2018-03-14 23:32:29 -0600

    Add files via upload

---
+++ DatosPersonales.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datospersonales;
+
+/**
+ *
+ * Ronald Alexis Miranda Miranda
+ */
+public class DatosPersonales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Nombre: Ronald Alexis");
+        System.out.println("Apellidos: Miranda Miranda");
+        System.out.println("Edad: 22 años");
+        System.out.println("Fecha de Nacimiento: 07-07-95");
+        System.out.println("Carnet: 00401617");
+        System.out.println("Direccion: La Paz,Olocuilta,Barrio El Calvario");
+        
+                
+           
+    }
+    
+}
